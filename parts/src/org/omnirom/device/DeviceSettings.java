9dd11a846d53926647e25d22ee9545aac9c6b03a
andr68rus <ashtmb@gmail.com>
2018-10-19 18:31:51 +0300

    msm8998-common: XiaomiParts: add Kernel Profiles toggle

--- parts/src/org/omnirom/device/DeviceSettings.java
+++ parts/src/org/omnirom/device/DeviceSettings.java
@@ -47,6 +47,8 @@ public class DeviceSettings extends PreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String KEY_CATEGORY_DISPLAY = "display";
+    private static final String SPECTRUM_KEY = "spectrum";
+    private static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
     public static final String S2S_KEY = "sweep2sleep";
     public static final String KEY_VIBSTRENGTH = "vib_strength";
     public static final String KEY_S2S_VIBSTRENGTH = "s2s_vib_strength";
@@ -61,6 +63,8 @@ public class DeviceSettings extends PreferenceFragment implements
     private S2SVibratorStrengthPreference mVibratorStrengthS2S;
     private ListPreference mS2S;
     private Preference mKcalPref;
+    private ListPreference mSPECTRUM;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.main, rootKey);
@@ -78,6 +82,12 @@ public class DeviceSettings extends PreferenceFragment implements
         mS2S = (ListPreference) findPreference(S2S_KEY);
         mS2S.setValue(Utils.getFileValue(FILE_S2S_TYPE, "0"));
         mS2S.setOnPreferenceChangeListener(this);
+
+        mSPECTRUM = (ListPreference) findPreference(SPECTRUM_KEY);
+        if( mSPECTRUM != null ) {
+            mSPECTRUM.setValue(SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, "0"));
+            mSPECTRUM.setOnPreferenceChangeListener(this);
+        }
 
         mVibratorStrength = (VibratorStrengthPreference) findPreference(KEY_VIBSTRENGTH);
         if (mVibratorStrength != null) {
@@ -113,7 +123,12 @@ public class DeviceSettings extends PreferenceFragment implements
             editor.putString(S2S_KEY, strvalue);
             editor.commit();
             return true;
+        } else if (SPECTRUM_KEY.equals(key)) {
+            strvalue = (String) newValue;
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, strvalue);
+            return true;
         }
+
         return true;
     }
 
