283699a2e074cceedfa9d4b61fe4f1e08026ebc7
ShevT <evgeny.shishkov@gmail.com>
2019-01-16 13:56:47 +0300

    msm8998-common: DeviceSettings: Clenup local static andriod libraries
    Change-Id: I01949f0f1a5679158edc9061ab5eb9aa0c7c606a

--- DeviceSettings/Android.mk
+++ DeviceSettings/Android.mk
@@ -20,8 +20,6 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v4 \
-    android-support-v13 \
     android-support-v7-recyclerview \
     android-support-v7-preference \
     android-support-v7-appcompat \
