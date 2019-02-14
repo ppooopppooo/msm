884195e913a0dc319d6e95332ca61ea544b4f81f
ShevT <evgeny.shishkov@gmail.com>
2019-02-14 15:55:26 +0300

    msm8998-common: DozeService: screenStateFilter

--- DeviceSettings/src/com/xiaomi/settings/doze/DozeService.java
+++ DeviceSettings/src/com/xiaomi/settings/doze/DozeService.java
@@ -38,8 +38,7 @@ public class DozeService extends Service {
         mProximitySensor = new ProximitySensor(this);
         mPickupSensor = new PickupSensor(this);
 
-        IntentFilter screenStateFilter = new IntentFilter();
-        screenStateFilter.addAction(Intent.ACTION_SCREEN_ON);
+        IntentFilter screenStateFilter = new IntentFilter(Intent.ACTION_SCREEN_ON);
         screenStateFilter.addAction(Intent.ACTION_SCREEN_OFF);
         registerReceiver(mScreenStateReceiver, screenStateFilter);
     }
