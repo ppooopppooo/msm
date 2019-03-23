3f86b25dc87196abc352e45421b6f6150c64de20
Artem Borisov <dedsa2002@gmail.com>
2019-03-23 13:26:38 +0530

    msm8998-common: Enable full dexpreopt
    Change-Id: I61e71a20bbfc17998bd0c8bade7ee98faad15524

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -147,11 +147,11 @@ BOARD_HEALTHD_CUSTOM_CHARGER_RES := $(PLATFORM_PATH)/charger/images
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
     endif
   endif
 endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # Display
 BOARD_USES_ADRENO := true
