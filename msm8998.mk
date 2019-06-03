a3d965cb7d49f3c3e0f0204a68a28bf2e84ede20
ppooopppooo <ppooopppooo@qq.com>
2019-06-03 17:07:19 +0800

    msm8998-common : add Lawnstep support

--- msm8998.mk
+++ msm8998.mk
@@ -242,7 +242,7 @@ PRODUCT_PACKAGES += \
     Lawnchair \
     LawnConf
 
- PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilts/etc/permissions/privapp-permissions-lawnchair.xml:system/etc/permissions/privapp-permissions-lawnchair.xml \
     $(LOCAL_PATH)/prebuilts/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml:system/etc/sysconfig/lawnchair-hiddenapi-package-whitelist.xml
 
